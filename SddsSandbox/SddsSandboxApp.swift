5b417cd4dd63bcbcdbcb923b3197d081693a7e87
Vladimir Kaltyrin <vkasci@gmail.com>
2024-03-21 15:38:33 +0300

    chore: Added an initial Xcode project structure. Added license, readme.

---
+++ SddsSandbox/SddsSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SddsSandboxApp.swift
+//  SddsSandbox
+//
+//  Created by Калтырин Владимир Александрович on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SddsSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
