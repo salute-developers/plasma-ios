3b1179c5b19841a1f23b247011c40d6c4d98ba37
Vladimir Kaltyrin <vkasci@gmail.com>
2024-03-21 15:38:33 +0300

    feat(sdds-icore): implemented building and archiving of XCFrameworks

---
+++ SDDSSwiftUI/SDDSSwiftUI/SDDSSwiftUI.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDDSSwiftUI.
+FOUNDATION_EXPORT double SDDSSwiftUIVersionNumber;
+
+//! Project version string for SDDSSwiftUI.
+FOUNDATION_EXPORT const unsigned char SDDSSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDDSSwiftUI/PublicHeader.h>
+
+
