a1a523d8127c61b19650eb0fe58d7b77c261dd81
Vladimir Kaltyrin <vkasci@gmail.com>
2024-04-01 18:47:40 +0300

    feat(sdds-icore): implemented building and archiving of XCFrameworks (#2)

---
+++ SDDSCore/SDDSCore/SDDSCore.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDDSCore.
+FOUNDATION_EXPORT double SDDSCoreVersionNumber;
+
+//! Project version string for SDDSCore.
+FOUNDATION_EXPORT const unsigned char SDDSCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDDSCore/PublicHeader.h>
+
+
---
+++ SDDSDemoApp/SDDSDemoApp/SDDSDemoApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import SDDSCore
+import SDDSUIKit
+import SDDSSwiftUI
+import SDDSIcons
+
+@main
+struct SDDSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
