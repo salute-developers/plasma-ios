9addf4ebcc119a1065abcb188df6b2326eaebdfa
Vladimir Kaltyrin <vkasci@gmail.com>
2024-04-11 15:40:29 +0300

    feat(sdds-theme-builder): implemented macOS CLI utility for the tokens generation

---
+++ SDDSThemeBuilder/SDDSThemeBuilder/Commands/PrepareCodeGenSchemaCommand.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+final class PrepareCodeGenSchemeCommand: Command {
+    override init(name: String = "Prepare scheme to generate tokens") {
+        super.init(name: name)
+    }
+    
+    override func run() -> CommandResult {
+        super.run()
+        
+        // Приготовление схемы для запуска кодогенерации
+    }
+}
