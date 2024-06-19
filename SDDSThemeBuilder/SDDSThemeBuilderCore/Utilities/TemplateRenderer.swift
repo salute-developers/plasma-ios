0bef58218f7461c2ddc2b731c55e1dd5efe73de4
Vladimir Kaltyrin <vkasci@gmail.com>
2024-06-19 18:10:19 +0300

    chore: fixed code validation

--- SDDSThemeBuilder/SDDSThemeBuilderCore/Utilities/TemplateRenderer.swift
+++ SDDSThemeBuilder/SDDSThemeBuilderCore/Utilities/TemplateRenderer.swift
@@ -22,8 +22,7 @@ class EnsureValueExistsNode: NodeType {
             print("Value for \(variableName) found: \(value)")
         } else {
             print("Value for \(variableName) not found in context \(flatContext)")
-            let _ = getValue(forKeyPath: variableName, from: flatContext)
-            //throw TemplateSyntaxError("Value for \(variableName) not found in context")
+            throw TemplateSyntaxError("Value for \(variableName) not found in context")
         }
         return ""
     }
@@ -86,6 +85,89 @@ class EnsureAnyValueExistsNode: NodeType {
     }
 }
 
+class EnsureFloatNonNegativeNode: NodeType {
+    let arguments: [String]
+    var token: Stencil.Token?
+
+    init(arguments: [String], token: Stencil.Token) {
+        self.arguments = arguments
+        self.token = token
+    }
+
+    func render(_ context: Context) throws -> String {
+        guard arguments.count == 1 else {
+            throw TemplateSyntaxError("ensure_float_non_negative tag requires exactly one argument")
+        }
+        let variableName = arguments[0]
+        
+        let flatContext = context.flatten()
+        if let value = getValue(forKeyPath: variableName, from: flatContext), let floatValue = value as? Float, floatValue >= 0 {
+            return ""
+        } else {
+            throw TemplateSyntaxError("Value for \(variableName) is not a non-negative float or not found in context")
+        }
+    }
+
+    private func getValue(forKeyPath keyPath: String, from dictionary: [String: Any]) -> Any? {
+        let keys = keyPath.split(separator: ".").map { String($0) }
+        var value: Any? = dictionary
+
+        for key in keys {
+            if let dict = value as? [String: Any], let nextValue = dict[key] {
+                value = nextValue
+            } else {
+                value = nil
+                break
+            }
+        }
+        
+        return value
+    }
+}
+
+class EnsureDoubleInRangeNode: NodeType {
+    let arguments: [String]
+    var token: Stencil.Token?
+
+    init(arguments: [String], token: Stencil.Token) {
+        self.arguments = arguments
+        self.token = token
+    }
+
+    func render(_ context: Context) throws -> String {
+        guard arguments.count == 3 else {
+            throw TemplateSyntaxError("ensure_double_in_range tag requires exactly three arguments")
+        }
+        let variableName = arguments[0]
+        guard let minValue = Double(arguments[1]), let maxValue = Double(arguments[2]) else {
+            throw TemplateSyntaxError("Invalid range for ensure_double_in_range")
+        }
+        
+        let flatContext = context.flatten()
+        if let value = getValue(forKeyPath: variableName, from: flatContext), let doubleValue = value as? Double, doubleValue >= minValue, doubleValue <= maxValue {
+            return ""
+        } else {
+            throw TemplateSyntaxError("Value for \(variableName) is not a double in range [\(minValue), \(maxValue)] or not found in context")
+        }
+    }
+
+    private func getValue(forKeyPath keyPath: String, from dictionary: [String: Any]) -> Any? {
+        let keys = keyPath.split(separator: ".").map { String($0) }
+        var value: Any? = dictionary
+
+        for key in keys {
+            if let dict = value as? [String: Any], let nextValue = dict[key] {
+                value = nextValue
+            } else {
+                value = nil
+                break
+            }
+        }
+        
+        return value
+    }
+}
+
 final class TemplateRenderer: Renderable {
     func render(context: [String: Any], template: StencilTemplate, templatesURL: URL) -> CommandResult {
         let ext = Extension()
@@ -96,6 +178,30 @@ final class TemplateRenderer: Renderable {
             }
             return value
         }
+        ext.registerFilter("ensure_double_in_range") { (value: Any?, arguments: [Any?]) in
+            let range = arguments.compactMap { value in
+                if let intValue = value as? Int {
+                    return Double(intValue)
+                } else if let doubleValue = value as? Double {
+                    return doubleValue
+                } else {
+                    return nil
+                }
+            }
+            guard range.count == 2 else {
+                throw TemplateSyntaxError("Invalid range for ensure_double_in_range")
+            }
+            guard let doubleValue = value as? Double, doubleValue >= range[0], doubleValue <= range[1] else {
+                throw TemplateSyntaxError("Value is not a double in range [\(range[0]),\(range[1])]")
+            }
+            return doubleValue
+        }
+        ext.registerFilter("ensure_float_non_negative") { (value: Any?) in
+             guard let floatValue = value as? Float, floatValue >= 0 else {
+                 throw TemplateSyntaxError("Value is not a non-negative float")
+             }
+             return floatValue
+        }
         ext.registerTag("ensure_value_exists") { parser, token in
             let arguments = token.components.dropFirst().map { $0 }
             return EnsureValueExistsNode(arguments: arguments, token: token)
@@ -104,21 +210,14 @@ final class TemplateRenderer: Renderable {
             let arguments = token.components.dropFirst().map { $0 }
             return EnsureAnyValueExistsNode(arguments: arguments, token: token)
         }
-        ext.registerFilter("ensure_double_in_range") { (value: Any?, arguments: [Any?]) in
-            guard let range = arguments.first as? [Double], range.count == 2 else {
-                throw TemplateSyntaxError("Invalid range for ensure_double_in_range")
-            }
-            guard let doubleValue = value as? Double, doubleValue >= range[0], doubleValue <= range[1] else {
-                throw TemplateSyntaxError("Value is not a double in range [\(range[0]),\(range[1])]")
-            }
-            return doubleValue
+        ext.registerTag("ensure_float_non_negative") { parser, token in
+             let arguments = token.components.dropFirst().map { $0 }
+             return EnsureFloatNonNegativeNode(arguments: arguments, token: token)
         }
-         ext.registerFilter("ensure_float_non_negative") { (value: Any?) in
-             guard let floatValue = value as? Float, floatValue >= 0 else {
-                 throw TemplateSyntaxError("Value is not a non-negative float")
-             }
-             return floatValue
-         }
+        ext.registerTag("ensure_double_in_range") { parser, token in
+            let arguments = token.components.dropFirst().map { $0 }
+            return EnsureDoubleInRangeNode(arguments: arguments, token: token)
+        }
 
         let templatesPath = templatesURL.absolutePath
         let stencilEnvironment = Environment(loader: FileSystemLoader(paths: [Path(templatesPath)]), extensions: [ext])
@@ -128,10 +227,12 @@ final class TemplateRenderer: Renderable {
         }
         
         // Генерация Swift файла
-        guard let rendered = try? template.render(context) else {
+        do {
+            let rendered = try template.render(context)
+            return .generated(rendered)
+        } catch {
+            print(error)
             return .error(CodeGenerationError.renderingFailed)
         }
-        
-        return .generated(rendered)
     }
 }
