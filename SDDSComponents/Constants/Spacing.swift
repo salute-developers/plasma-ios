1f3492b5a6e8581aa041438ba20f5eafcc53d24a
Vladimir Kaltyrin <vkasci@gmail.com>
2024-07-08 15:01:55 +0300

    chore: removed unused constants

--- SDDSComponents/Constants/Spacing.swift
+++ SDDSComponents/Constants/Spacing.swift
@@ -4,13 +4,4 @@ import SwiftUI
 enum Spacing {
     static let four: CGFloat = 4
     static let eight: CGFloat = 8
-    static let ten: CGFloat = 10
-    static let eleven: CGFloat = 11
-    static let twelve: CGFloat = 12
-    static let sixteen: CGFloat = 16
-    static let twenty: CGFloat = 20
-    static let twentyTwo: CGFloat = 22
-    static let twentyFour: CGFloat = 24
-    static let five: CGFloat = 5
-    static let fourty: CGFloat = 40
 }
