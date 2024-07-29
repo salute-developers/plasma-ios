4af32a5e418cdd81ca4a8ce25a253c9b50f40216
Vladimir Kaltyrin <vkasci@gmail.com>
2024-07-29 11:32:03 +0300

    chore: fixed padding

--- SDDSComponents/Views/HierarchicalList/HierarchicalList.swift
+++ SDDSComponents/Views/HierarchicalList/HierarchicalList.swift
@@ -5,9 +5,9 @@ import SwiftUI
  `HierarchicalList` представляет собой контейнер для отображения иерархической структуры с элементами, которые могут быть любыми вью.
  
  - Parameters:
-    - items: Массив элементов, содержащих данные и представление.
-    - horizontalIndent: Отступ слева для каждого уровня иерархии, начиная со второго элемента.
-    - verticalSpacing: Вертикальное расстояние между элементами.
+ - items: Массив элементов, содержащих данные и представление.
+ - horizontalIndent: Отступ слева для каждого уровня иерархии, начиная со второго элемента.
+ - verticalSpacing: Вертикальное расстояние между элементами.
  */
 struct HierarchicalList<Data: Hashable, Content: View>: View {
     let data: [Data]
@@ -16,14 +16,10 @@ struct HierarchicalList<Data: Hashable, Content: View>: View {
     let content: (Data) -> Content
     
     var body: some View {
-        VStack(spacing: verticalSpacing) {
+        VStack(alignment: .leading, spacing: verticalSpacing) {
             ForEach(Array(data.enumerated()), id: \.element) { index, item in
-                HStack {
-                    if index > 0 {
-                        Spacer().frame(width: horizontalIndent)
-                    }
-                    content(item)
-                }
+                content(item)
+                    .padding(.leading, index == 0 ? 0 : horizontalIndent)
             }
         }
     }
@@ -33,11 +29,11 @@ struct HierarchicalList<Data: Hashable, Content: View>: View {
  `HierarchicalListItem` представляет собой элемент иерархической структуры, содержащий данные и представление.
  
  - Properties:
-    - view: Вью, которое будет отображаться в списке.
+ - view: Вью, которое будет отображаться в списке.
  */
 struct HierarchicalListItem<ItemView: View> {
     public let view: ItemView
-
+    
     public init(view: ItemView) {
         self.view = view
     }
