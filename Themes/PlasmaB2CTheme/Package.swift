d47620a5a382a184059489acd665717db7a2436c
Vladimir Kaltyrin <vkasci@gmail.com>
2024-08-02 13:54:43 +0300

    feat(sdds-uikit): added Package.swift for PlasmaB2CTheme (#69)

---
+++ Themes/PlasmaB2CTheme/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "PlasmaB2CTheme",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "PlasmaB2CTheme",
+            targets: ["PlasmaB2CTheme"]),
+    ],
+    dependencies: [
+        .package(name: "SDDSThemeCore", path: "../../SDDSThemeBuilder/SDDSThemeCore")
+    ],
+    targets: [
+        .target(
+            name: "PlasmaB2CTheme",
+            dependencies: ["SDDSThemeCore"],
+            path: ".",
+            exclude: ["PlasmaB2CTheme.xcodeproj", "SDDSTheme.h"],
+            resources: [
+                .process("Fonts")
+            ]
+        )
+    ]
+)
