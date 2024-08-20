f29efa4d352aa2a0986eaccaa51f57b69e389108
Vladimir Kaltyrin <vkasci@gmail.com>
2024-08-20 11:17:56 +0300

    fix(sdds-core): fixed project structure and versions

---
+++ SDDSThemeBuilder/SDDSThemeUtilities/Sources/SDDSThemeUtilities/SDDSThemeUtilities.h
@@ -0,0 +1,18 @@
+//
+//  SDDSThemeUtilities.h
+//  SDDSThemeUtilities
+//
+//  Created by Vladimir Kaltyrin on 15.08.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDDSThemeUtilities.
+FOUNDATION_EXPORT double SDDSThemeUtilitiesVersionNumber;
+
+//! Project version string for SDDSThemeUtilities.
+FOUNDATION_EXPORT const unsigned char SDDSThemeUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDDSThemeUtilities/PublicHeader.h>
+
+
