4a707e835e6eb64df2360846573a79f54dd7afad
Vladimir Kaltyrin <vkasci@gmail.com>
2024-10-21 19:10:26 +0300

    feat(sdds-demo):added firebase distribution

--- SDDSDemoApp/SDDSDemoApp/SDDSDemoApp.swift
+++ SDDSDemoApp/SDDSDemoApp/SDDSDemoApp.swift
@@ -1,8 +1,20 @@
 import SwiftUI
 import SDDSServTheme
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct SDDSDemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ComponentsView()
