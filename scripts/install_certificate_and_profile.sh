f2c64b0623f28d38e61e071f0e9fa941884e2ab6
Димитраки Владимир <dimitraki666@gmail.com>
2024-12-10 15:05:20 +0300

    fix(testflight_build): set debug variables in script

--- scripts/install_certificate_and_profile.sh
+++ scripts/install_certificate_and_profile.sh
@@ -7,6 +7,7 @@ KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
 
 # import certificate and provisioning profile from secrets
 echo -n "$BUILD_CERTIFICATE_BASE64" | base64 --decode -o $CERTIFICATE_PATH
+echo -n "$BUILD_PROVISION_PROFILE_BASE64" | base64 --decode -o "$PP_PATH"
 
 # create temporary keychain
 security create-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
@@ -17,3 +18,10 @@ security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
 security import $CERTIFICATE_PATH -P "$P12_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
 security set-key-partition-list -S apple-tool:,apple: -k "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
 security list-keychain -d user -s $KEYCHAIN_PATH
+
+# verify certificate import
+security find-identity -v -p codesigning $KEYCHAIN_PATH
+
+# verify provisioning profile
+ls -l "$PP_PATH"
+security cms -D -i "$PP_PATH"
