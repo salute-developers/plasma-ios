7065399475680fcc3601392bb0aea95731316efa
Димитраки Владимир <dimitraki666@gmail.com>
2025-02-19 20:51:09 +0300

    feat: added image-context-builder

--- Themes/SDDSservTheme/Cell/Cell+BaseVariations.swift
+++ Themes/SDDSservTheme/Cell/Cell+BaseVariations.swift
@@ -100,7 +100,6 @@ public struct CellVariation {
 private extension CellAppearance {
     static var base: CellAppearance {
         var appearance = CellAppearance()
-        appearance.disclosureImageColor = ColorToken.textDefaultSecondary
         appearance.disclosureTextColor = ColorToken.textDefaultSecondary
         appearance.labelColor = ColorToken.textDefaultSecondary
         appearance.subtitleColor = ColorToken.textDefaultSecondary
