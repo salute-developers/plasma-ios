83cb32904d064b9d0158e7aaadc31992f085e004
Димитраки Владимир <dimitraki666@gmail.com>
2025-02-20 09:35:12 +0300

    chore: uncommented textfield-preview and textarea

--- SDDSComponents/SDDSComponentsPreview/Components/SDDSCell/CellPreview.swift
+++ SDDSComponents/SDDSComponentsPreview/Components/SDDSCell/CellPreview.swift
@@ -2,11 +2,9 @@ import Foundation
 import SwiftUI
 import SDDSComponents
 import SDDSServTheme
-import SDDSIcons
 
 struct CellPreview: PreviewProvider {
     static var previews: some View {
-<<<<<<< HEAD
         HStack {
             SDDSCell(
                 appearance: Cell.s.appearance,
@@ -27,52 +25,5 @@ struct CellPreview: PreviewProvider {
         }
         .previewLayout(PreviewLayout.sizeThatFits)
         .previewDisplayName("Cell")
-=======
-        Cell(
-            appearance: Cell.l.appearance,
-            alignment: .center,
-            label: "label",
-            title: "title",
-            subtitle: "subtitle",
-            disclosureEnabled: false,
-            disclosureImage: nil,
-            disclosureText: "",
-            leftContent: {
-                avatar
-                    .environment(\.avatarAppearance, SDDSAvatar.default.large.appearance)
-            },
-            centerContent: {},
-            rightContent: {
-                avatar
-                    .environment(\.avatarAppearance, SDDSAvatar.default.medium.appearance)
-                avatar
-                    .environment(\.avatarAppearance, SDDSAvatar.default.medium.appearance)
-            },
-            disclosure: {}
-        )
-        .previewLayout(PreviewLayout.sizeThatFits)
-        .previewDisplayName("Cell without disclosure")
-        
-        Cell(
-            appearance: Cell.s.appearance,
-            alignment: .center,
-            label: "label",
-            title: "title",
-            subtitle: "subtitle",
-            disclosureEnabled: true,
-            disclosureText: "disclosure",
-            leftContent: {
-                avatar
-                    .environment(\.avatarAppearance, SDDSAvatar.default.large.appearance)
-            },
-            centerContent: {},
-            rightContent: {
-                iconButton
-            },
-            disclosure: {}
-        )
-        .previewLayout(PreviewLayout.sizeThatFits)
-        .previewDisplayName("Cell with disclosure")
->>>>>>> a7ec33d0 (feat: added selection appearance in avatar)
     }
 }
