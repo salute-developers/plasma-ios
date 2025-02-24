27b2c75b771dacb6c168c155fd62c019c881111e
Димитраки Владимир <dimitraki666@gmail.com>
2025-02-24 11:58:40 +0300

    chore: remove unused cases in components in themebuilder

--- SDDSThemeBuilder/SDDSThemeBuilderCore/Model/Components/ComponentConfiguration/Component.swift
+++ SDDSThemeBuilder/SDDSThemeBuilderCore/Model/Components/ComponentConfiguration/Component.swift
@@ -3,12 +3,6 @@ import Foundation
 enum Component: String {
     case embeddedChip = "embedded-chip"
     case chipGroup = "chip-group"
-    case avatar = "avatar"
-    case iconButton = "icon-button"
-    case checkBox = "check-box"
-    case radioBox = "radio-box"
-    case `switch` = "switch"
-    case cell = "cell"
     
     var componentName: String {
         switch self {
@@ -16,18 +10,6 @@ enum Component: String {
             return "EmbeddedChip"
         case .chipGroup:
             return "ChipGroup"
-        case .avatar:
-            return "Avatar"
-        case .iconButton:
-            return "IconButton"
-        case .checkBox:
-            return "Checkbox"
-        case .radioBox:
-            return "Radiobox"
-        case .switch:
-            return "Switch"
-        case .cell:
-            return "Cell"
         }
     }
 }
