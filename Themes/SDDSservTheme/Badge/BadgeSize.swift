b51d69726d6cb46e4d32a6dd660542109736abd1
Vladimir Kaltyrin <vkasci@gmail.com>
2025-02-25 17:00:48 +0300

    feat: added badge to demo app

--- Themes/SDDSservTheme/Badge/BadgeSize.swift
+++ Themes/SDDSservTheme/Badge/BadgeSize.swift
@@ -18,11 +18,11 @@ struct BadgeSize {
 }
 struct BadgeSizeL: BadgeSizeConfiguration {
     var cornerRadius = ShapeToken.roundS.cornerRadius
-    var endContentPadding = CGFloat(16.0)
+    var endContentPadding = CGFloat(4.0)
     var endContentSize = CGSize(width:16.0, height:16.0)
     var endPadding = CGFloat(11.0)
     var height = CGFloat(28.0)
-    var startContentPadding = CGFloat(16.0)
+    var startContentPadding = CGFloat(4.0)
     var startContentSize = CGSize(width:16.0, height:16.0)
     var startPadding = CGFloat(11.0)
     public var debugDescription: String {
@@ -31,11 +31,11 @@ struct BadgeSizeL: BadgeSizeConfiguration {
 }
 struct BadgeSizeM: BadgeSizeConfiguration {
     var cornerRadius = ShapeToken.roundXs.cornerRadius
-    var endContentPadding = CGFloat(12.0)
+    var endContentPadding = CGFloat(3.0)
     var endContentSize = CGSize(width:12.0, height:12.0)
     var endPadding = CGFloat(9.0)
     var height = CGFloat(24.0)
-    var startContentPadding = CGFloat(12.0)
+    var startContentPadding = CGFloat(3.0)
     var startContentSize = CGSize(width:12.0, height:12.0)
     var startPadding = CGFloat(9.0)
     public var debugDescription: String {
@@ -44,11 +44,11 @@ struct BadgeSizeM: BadgeSizeConfiguration {
 }
 struct BadgeSizeS: BadgeSizeConfiguration {
     var cornerRadius = ShapeToken.roundXs.cornerRadius
-    var endContentPadding = CGFloat(12.0)
+    var endContentPadding = CGFloat(2.0)
     var endContentSize = CGSize(width:12.0, height:12.0)
     var endPadding = CGFloat(7.0)
     var height = CGFloat(20.0)
-    var startContentPadding = CGFloat(12.0)
+    var startContentPadding = CGFloat(2.0)
     var startContentSize = CGSize(width:12.0, height:12.0)
     var startPadding = CGFloat(7.0)
     public var debugDescription: String {
@@ -57,11 +57,11 @@ struct BadgeSizeS: BadgeSizeConfiguration {
 }
 struct BadgeSizeXs: BadgeSizeConfiguration {
     var cornerRadius = ShapeToken.roundXxs.cornerRadius
-    var endContentPadding = CGFloat(10.0)
+    var endContentPadding = CGFloat(2.0)
     var endContentSize = CGSize(width:10.0, height:10.0)
     var endPadding = CGFloat(4.0)
     var height = CGFloat(16.0)
-    var startContentPadding = CGFloat(10.0)
+    var startContentPadding = CGFloat(2.0)
     var startContentSize = CGSize(width:10.0, height:10.0)
     var startPadding = CGFloat(4.0)
     public var debugDescription: String {
