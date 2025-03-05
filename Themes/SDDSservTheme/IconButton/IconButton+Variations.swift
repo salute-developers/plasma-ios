6796c81444198eb27b0e31a6c2f26feb0ce00423
Димитраки Владимир <dimitraki666@gmail.com>
2025-03-05 21:02:18 +0300

    feat: added new sdds-button variations

--- Themes/SDDSservTheme/IconButton/IconButton+Variations.swift
+++ Themes/SDDSservTheme/IconButton/IconButton+Variations.swift
@@ -292,6 +292,102 @@ public extension GeneralAppearanceVariation<IconButton, ButtonAppearance, IconBu
     }
 }
 
+public extension GeneralAppearanceVariation<IconButton, ButtonAppearance, IconButtonVariation.Xl> {
+    
+    var pilled: ComponentAppearanceVariation<IconButton, ButtonAppearance> {
+        var size =  IconButtonAnySize(size: appearance.size)
+        size.cornerRadius = size.height / 2
+
+        var appearance = appearance
+        appearance.size = size
+    
+        return .init(
+            name: "xl.pilled",
+            appearance: appearance
+        )
+    }
+    
+    var `default`: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "`default`",
+            appearance: appearance.`default`
+        )
+    }
+    var accent: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "accent",
+            appearance: appearance.accent
+        )
+    }
+    var black: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "black",
+            appearance: appearance.black
+        )
+    }
+    var clear: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "clear",
+            appearance: appearance.clear
+        )
+    }
+    var dark: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "dark",
+            appearance: appearance.dark
+        )
+    }
+    var negative: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "negative",
+            appearance: appearance.negative
+        )
+    }
+    var positive: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "positive",
+            appearance: appearance.positive
+        )
+    }
+    var secondary: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "secondary",
+            appearance: appearance.secondary
+        )
+    }
+    var warning: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "warning",
+            appearance: appearance.warning
+        )
+    }
+    var white: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "white",
+            appearance: appearance.white
+        )
+    }
+    var variation: Variation<Appearance> {
+        .init(
+            originalVariation: self,
+            styles: [
+                self.`default`,
+                self.accent,
+                self.black,
+                self.clear,
+                self.dark,
+                self.negative,
+                self.positive,
+                self.secondary,
+                self.warning,
+                self.white,
+            ],
+            name: name,
+            appearance: appearance
+        )
+    }
+}
+
 public extension GeneralAppearanceVariation<IconButton, ButtonAppearance, IconButtonVariation.Xs> {
     
     var pilled: ComponentAppearanceVariation<IconButton, ButtonAppearance> {
@@ -303,6 +399,102 @@ public extension GeneralAppearanceVariation<IconButton, ButtonAppearance, IconBu
     
         return .init(
             name: "xs.pilled",
+            appearance: appearance
+        )
+    }
+    
+    var `default`: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "`default`",
+            appearance: appearance.`default`
+        )
+    }
+    var accent: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "accent",
+            appearance: appearance.accent
+        )
+    }
+    var black: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "black",
+            appearance: appearance.black
+        )
+    }
+    var clear: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "clear",
+            appearance: appearance.clear
+        )
+    }
+    var dark: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "dark",
+            appearance: appearance.dark
+        )
+    }
+    var negative: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "negative",
+            appearance: appearance.negative
+        )
+    }
+    var positive: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "positive",
+            appearance: appearance.positive
+        )
+    }
+    var secondary: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "secondary",
+            appearance: appearance.secondary
+        )
+    }
+    var warning: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "warning",
+            appearance: appearance.warning
+        )
+    }
+    var white: AppearanceVariation<ButtonAppearance> {
+        return .init(
+            name: "white",
+            appearance: appearance.white
+        )
+    }
+    var variation: Variation<Appearance> {
+        .init(
+            originalVariation: self,
+            styles: [
+                self.`default`,
+                self.accent,
+                self.black,
+                self.clear,
+                self.dark,
+                self.negative,
+                self.positive,
+                self.secondary,
+                self.warning,
+                self.white,
+            ],
+            name: name,
+            appearance: appearance
+        )
+    }
+}
+
+public extension GeneralAppearanceVariation<IconButton, ButtonAppearance, IconButtonVariation.Xxs> {
+    
+    var pilled: ComponentAppearanceVariation<IconButton, ButtonAppearance> {
+        var size =  IconButtonAnySize(size: appearance.size)
+        size.cornerRadius = size.height / 2
+
+        var appearance = appearance
+        appearance.size = size
+    
+        return .init(
+            name: "xxs.pilled",
             appearance: appearance
         )
     }
