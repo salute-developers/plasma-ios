8cfe5de434cc612c4553c1ab32c7926f83f6766c
Димитраки Владимир <dimitraki666@gmail.com>
2025-03-12 16:04:28 +0300

    fix: fixed cell generator

--- SDDSThemeBuilder/SDDSThemeBuilderCore/Model/Components/ComponentConfiguration/Component.swift
+++ SDDSThemeBuilder/SDDSThemeBuilderCore/Model/Components/ComponentConfiguration/Component.swift
@@ -3,6 +3,11 @@ import Foundation
 enum Component: String {
     case embeddedChip = "embedded-chip"
     case chipGroup = "chip-group"
+    case avatar = "avatar"
+    case checkbox = "check-box"
+    case radiobox = "radio-box"
+    case `switch` = "switch"
+    case iconButton = "icon-button"
     
     var componentName: String {
         switch self {
@@ -10,6 +15,16 @@ enum Component: String {
             return "EmbeddedChip"
         case .chipGroup:
             return "ChipGroup"
+        case .avatar:
+            return "Avatar"
+        case .checkbox:
+            return "Checkbox"
+        case .radiobox:
+            return "Radiobox"
+        case .switch:
+            return "Switch"
+        case .iconButton:
+            return "IconButton"
         }
     }
 }
