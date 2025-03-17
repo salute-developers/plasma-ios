a1c3c9fca1560b607ddfa565ddd54b17d1729486
Vladimir Kaltyrin <vkasci@gmail.com>
2025-03-17 10:46:22 +0300

    chore: tests

---
+++ SDDSDemoApp/Snapshot Tests/Snapshot_tests.swift
@@ -0,0 +1,45 @@
+//
+//  Snapshot_tests.swift
+//  Snapshot tests
+//
+//  Created by Vladimir Kaltyrin on 14.03.2025.
+//
+
+import XCTest
+import SwiftUI
+import UIKit
+import SDDSServTheme
+@testable import SDDSDemoApp
+@testable import SDDSComponents
+@testable import SDSnapshots
+
+final class Snapshot_tests: XCTestCase {
+    private let deviceGroup: SnapshotDeviceGroup = (.phone + .tablet).universal
+
+    func test_cellView() async {
+        await Xct.snapshotAsync(mode: .record, deviceGroup: deviceGroup) { device in
+            let viewModel = CellViewModel()
+            viewModel.label = "Hello, World!"
+            let view = CellView(viewModel: viewModel)
+            
+            let sut = UIHostingController(rootView: view)
+            return sut
+        }
+    }
+    
+    func test_button_component() async {
+        await Xct.snapshotAsync(mode: .record, deviceGroup: deviceGroup) { device in
+            let view = SDDSButton(
+                title: "title",
+                subtitle: "subtitle",
+                appearance: BasicButton.m.default.appearance,
+                action: {}
+            )
+            view.environment(\.colorScheme, .dark)
+            
+            let sut = UIHostingController(rootView: view)
+            return sut
+        }
+    }
+
+}
