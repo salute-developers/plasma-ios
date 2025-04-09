852e710a8f8f5dd43edb73b7504ef5bc8a96e1be
Димитраки Владимир <dimitraki666@gmail.com>
2025-04-09 15:32:57 +0300

    chore: add computed states in button-context builder

--- SDDSThemeBuilder/SDDSThemeBuilderCore/CodeGeneration/Сomponents/ContextBuilders/ButtonColorContextBuilder.swift
+++ SDDSThemeBuilder/SDDSThemeBuilderCore/CodeGeneration/Сomponents/ContextBuilders/ButtonColorContextBuilder.swift
@@ -5,7 +5,7 @@ final class ButtonColorContextBuilder: CodeGenerationContextBuilder {
     let highlightedColor: ColorState?
     let hoveredColor: ColorState?
     
-    init(defaultColor: ColorKeyValue?, highlightedColor: ColorState?, hoveredColor: ColorState? = nil) {
+    init(defaultColor: ColorKeyValue?, highlightedColor: ColorState? = nil, hoveredColor: ColorState? = nil) {
         self.defaultColor = defaultColor
         self.highlightedColor = highlightedColor
         self.hoveredColor = hoveredColor
@@ -15,17 +15,26 @@ final class ButtonColorContextBuilder: CodeGenerationContextBuilder {
         if defaultColor == nil, highlightedColor == nil, hoveredColor == nil {
             return nil
         }
-         
-        let defaultColorToken = ColorTokenContextBuilder(defaultColor, hasDefault: true).context ?? ""
-        let highlightedColorToken = ColorTokenContextBuilder(highlightedColor, hasDefault: true).context ?? ""
-        let hoveredColorToken = ColorTokenContextBuilder(hoveredColor, hasDefault: true).context ?? ""
         
-        if hoveredColor == nil {
-            return "ButtonColor(defaultColor: \(defaultColorToken), highlightedColor: \(highlightedColorToken)"
+        let allStates: [(String, ColorState?)] = [
+            ("highlightedColor", highlightedColor),
+           ( "hoveredColor", hoveredColor)
+        ]
+        
+        let buttonColorString = """
+        ButtonColor(defaultColor: \(ColorTokenContextBuilder(defaultColor, hasDefault: true).context ?? "")
+        """
+        
+        let colorStatesString = allStates.compactMap { (stateColorString, stateColor) -> String? in
+            guard stateColor != nil else {
+                return nil
+            }
+            let colorToken = ColorTokenContextBuilder(stateColor, hasDefault: true).context ?? ""
+            return ", \(stateColorString): \(colorToken)"
         }
         
-        return """
-        ButtonColor(defaultColor: \(defaultColorToken), highlightedColor: \(highlightedColorToken), hoveredColor: \(hoveredColorToken))
-        """
+        let combinedColorStatesString = colorStatesString.joined()
+        
+        return buttonColorString + combinedColorStatesString + ")"
     }
 }
