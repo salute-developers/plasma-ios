578d00ec713fe532909f0acb6d734986e69ebef4
Ангир Булинов <aabulinov@sberbank.ru>
2025-05-20 15:16:16 +0300

    test(sdds-acore/testing): Added new snapshot tests

--- SDDSDemoApp/SDDSDemoAppTests/ChipTestCase/ChipTestCases.swift
+++ SDDSDemoApp/SDDSDemoAppTests/ChipTestCase/ChipTestCases.swift
@@ -79,3 +79,157 @@ struct ChipSizeLDisabled: View {
         )
     }
 }
+
+struct ChipGroupLDense: View {
+    var body: some View {
+        let chipData = (0..<5).map { _ in
+            ChipData(
+                title: "Label",
+                isEnabled: true,
+                iconImage: nil,
+                buttonImage: nil,
+                appearance: Chip.l.default.appearance,
+                accessibility: ChipAccessibility(),
+                removeAction: {}
+            )
+        }
+        
+        SDDSChipGroup(
+            data: chipData,
+            appearance: ChipGroupDense.l.default.appearance
+        )
+        .frame(width: 375, height: 120, alignment: .top)
+    }
+}
+
+struct ChipGroupMSecondary: View {
+    var body: some View {
+        let chipData = (0..<20).map { _ in
+            ChipData(
+                title: "Label",
+                isEnabled: true,
+                iconImage: nil,
+                buttonImage: nil,
+                appearance: Chip.m.secondary.appearance,
+                accessibility: ChipAccessibility(),
+                removeAction: {}
+            )
+        }
+        
+        SDDSChipGroup(
+            data: chipData,
+            appearance: ChipGroupWide.m.secondary.appearance
+        )
+        .frame(width: 375, height: 250, alignment: .top)
+    }
+}
+
+struct ChipGroupSAccent: View {
+    var body: some View {
+        let chipData = (0..<20).map { _ in
+            ChipData(
+                title: "Label",
+                isEnabled: true,
+                iconImage: nil,
+                buttonImage: nil,
+                appearance: Chip.s.accent.appearance,
+                accessibility: ChipAccessibility(),
+                removeAction: {}
+            )
+        }
+        
+        SDDSChipGroup(
+            data: chipData,
+            appearance: ChipGroupDense.s.accent.appearance
+        )
+        .frame(width: 375, height: 120, alignment: .top)
+    }
+}
+
+struct ChipGroupXs: View {
+    var body: some View {
+        let chipData = (0..<5).map { _ in
+            ChipData(
+                title: "Label",
+                isEnabled: true,
+                iconImage: nil,
+                buttonImage: nil,
+                appearance: Chip.xs.default.appearance,
+                accessibility: ChipAccessibility(),
+                removeAction: {}
+            )
+        }
+        
+        SDDSChipGroup(
+            data: chipData,
+            appearance: ChipGroupDense.xs.default.appearance
+        )
+        .frame(width: 275, height: 20, alignment: .top)
+    }
+}
+
+struct ChipGroupLSecondaryPilled: View {
+    var body: some View {
+        let chipData = (0..<5).map { _ in
+            ChipData(
+                title: "Label",
+                isEnabled: true,
+                iconImage: Image.image("chipIcon"),
+                buttonImage: Image.image("chipClose"),
+                appearance: Chip.l.pilled.secondary.appearance,
+                accessibility: ChipAccessibility(),
+                removeAction: {}
+            )
+        }
+        
+        SDDSChipGroup(
+            data: chipData,
+            appearance: ChipGroupDense.l.pilled.secondary.appearance
+        )
+        .frame(width: 375, height: 140, alignment: .top)
+    }
+}
+
+struct ChipGroupMAccentWide: View {
+    var body: some View {
+        let chipData = (0..<5).map { _ in
+            ChipData(
+                title: "Label",
+                isEnabled: true,
+                iconImage: nil,
+                buttonImage: Image.image("chipClose"),
+                appearance: Chip.m.accent.appearance,
+                accessibility: ChipAccessibility(),
+                removeAction: {}
+            )
+        }
+        
+        SDDSChipGroup(
+            data: chipData,
+            appearance: ChipGroupWide.m.accent.appearance
+        )
+        .frame(width: 375, height: 120, alignment: .top)
+    }
+}
+
+struct ChipGroupLSecondaryHasClose: View {
+    var body: some View {
+        let chipData = (0..<5).map { _ in
+            ChipData(
+                title: "Label",
+                isEnabled: true,
+                iconImage: nil,
+                buttonImage: Image.image("chipClose"),
+                appearance: Chip.l.secondary.appearance,
+                accessibility: ChipAccessibility(),
+                removeAction: {}
+            )
+        }
+        
+        SDDSChipGroup(
+            data: chipData,
+            appearance: ChipGroupDense.l.secondary.appearance
+        )
+        .frame(width: 375, height: 120, alignment: .top)
+    }
+}
