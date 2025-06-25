c46db01a12f77732755b75bb14957f4b319c9424
Ангир Булинов <aabulinov@sberbank.ru>
2025-06-25 09:51:17 +0300

    test(sdds-acore/testing) Card snapshot tests

--- SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/CheckBoxSnapshotTest.swift
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/CheckBoxSnapshotTest.swift
@@ -15,42 +15,52 @@ import SwiftUI
 
 final class CheckBoxSnapshotTest: XCTestCase {
     
+    @MainActor
     func testCheckBoxSizeLNegative() async throws {
         try await runSnapshotTest(view: CheckBoxSizeLNegative())
     }
     
+    @MainActor
     func testCheckBoxSizeLNegativeUnchecked() async throws {
         try await runSnapshotTest(view: CheckBoxSizeLNegativeUnchecked())
     }
     
+    @MainActor
     func testCheckBoxSizeM() async throws {
         try await runSnapshotTest(view: CheckBoxSizeM())
     }
     
+    @MainActor
     func testCheckBoxSizeS() async throws {
         try await runSnapshotTest(view: CheckBoxSizeS())
     }
     
+    @MainActor
     func testCheckBoxSizeSDisabled() async throws {
         try await runSnapshotTest(view: CheckBoxSizeSDisabled())
     }
     
+    @MainActor
     func testCheckBoxSizeMIndeterminate() async throws {
         try await runSnapshotTest(view: CheckBoxSizeMIndeterminate())
     }
     
+    @MainActor
     func testCheckBoxSizeMNoLabelNoDescription() async throws {
         try await runSnapshotTest(view: CheckBoxSizeMNoLabelNoDescription())
     }
     
+    @MainActor
     func testCheckBoxGroupSizeM() async throws {
         try await runSnapshotTest(view: CheckBoxGroupSizeM())
     }
     
+    @MainActor
     func testCheckBoxGroupSizeS() async throws {
         try await runSnapshotTest(view: CheckBoxGroupSizeS())
     }
     
+    @MainActor
     func testCheckBoxGroupSizeMDisabled() async throws {
         try await runSnapshotTest(view: CheckBoxGroupSizeMDisabled())
     }
