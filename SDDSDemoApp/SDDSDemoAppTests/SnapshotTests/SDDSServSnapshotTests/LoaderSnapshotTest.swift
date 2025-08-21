594875a4196374c872bd46051ec4c16ef4a0c3f5
angirb <bulinoff@gmail.com>
2025-08-21 15:15:08 +0300

    test(sdds-acore/testing): Loader snapshot tests

---
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/SDDSServSnapshotTests/LoaderSnapshotTest.swift
@@ -0,0 +1,32 @@
+//
+//  LoaderSnapshotTest.swift
+//  SDDSDemoApp
+//
+//  Created by Булинов Ангрик Александрович on 21.08.2025.
+//
+
+@testable import SDSnapshots
+@testable import SDDSDemoApp
+@testable import SDDSComponents
+@testable import SDDSIcons
+import XCTest
+import SDDSServTheme
+import UIKit
+import SwiftUI
+
+final class LoaderSnapshotTest: XCTestCase {
+    
+    @MainActor
+    func testLoaderCaseSpinner() async throws {
+        let appearance = LoaderAppearance(spinnerAppearance: Spinner.l.default.appearance)
+        
+        try await runSnapshotTest(view: LoaderCaseSpinner(appearance: appearance))
+    }
+    
+    @MainActor
+    func testLoaderCaseCircularProgress() async throws {
+        let appearance = LoaderAppearance(circularProgressAppearance: CircularProgressBar.l.default.appearance)
+        
+        try await runSnapshotTest(view: LoaderCaseCircularProgress(appearance: appearance))
+    }
+}
