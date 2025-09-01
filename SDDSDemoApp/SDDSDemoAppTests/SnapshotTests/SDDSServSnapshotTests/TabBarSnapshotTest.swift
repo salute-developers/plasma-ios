ef6d8a14d71f7eb8f541c092fdaa788c25f174db
angirb <bulinoff@gmail.com>
2025-09-01 14:10:52 +0300

    test(sdds-acore/testing): TabBar snapshot tests

---
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/SDDSServSnapshotTests/TabBarSnapshotTest.swift
@@ -0,0 +1,144 @@
+//
+//  TabBarSnapshotTest.swift
+//  SDDSDemoApp
+//
+//  Created by Булинов Ангрик Александрович on 01.09.2025.
+//
+
+@testable import SDSnapshots
+@testable import SDDSDemoApp
+@testable import SDDSComponents
+@testable import SDDSIcons
+import XCTest
+import SDDSServTheme
+import UIKit
+import SwiftUI
+
+final class TabBarSnapshotTest: XCTestCase {
+    
+    @MainActor
+    func testTabBarClearLDefaultExtraTypeNone() async throws {
+        let appearance = TabBarClear.l.default.appearance
+
+        try await runSnapshotTest(view: TabBarClearLDefaultExtraTypeNone(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarHasLabelClearMSecondaryCounter() async throws {
+        let appearance = TabBarHasLabelClear.m.secondary.appearance
+        
+        try await runSnapshotTest(view: TabBarHasLabelClearMSecondaryCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarHasLabelSolidLAccentIndicator() async throws {
+        let appearance = TabBarHasLabel.l.accent.appearance
+        
+        try await runSnapshotTest(view: TabBarHasLabelSolidLAccentIndicator(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarIslandClearLShadowDefault() async throws {
+        let appearance = TabBarIslandClear.l.shadow.default.appearance
+        
+        try await runSnapshotTest(view: TabBarIslandClearLShadowDefault(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarIslandHasLabelClearLShadowSecondaryCounter() async throws {
+        let appearance = TabBarIslandHasLabelClear.l.shadow.secondary.appearance
+        
+        try await runSnapshotTest(view: TabBarIslandHasLabelClearLShadowSecondaryCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarHasLabelSolidLShadowAccentCounter() async throws {
+        let appearance = TabBarHasLabel.l.shadow.accent.appearance
+        
+        try await runSnapshotTest(view: TabBarHasLabelSolidLShadowAccentCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarIslandSolidLDefaultCounter() async throws {
+        let appearance = TabBarIsland.l.default.appearance
+        
+        try await runSnapshotTest(view: TabBarIslandSolidLDefaultCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarSolidLRoundedDefaultIndicator() async throws {
+        let appearance = TabBar.l.rounded.default.appearance
+        
+        try await runSnapshotTest(view: TabBarSolidLRoundedDefaultIndicator(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarClearMRoundedSecondaryCounter() async throws {
+        let appearance = TabBarClear.m.rounded.secondary.appearance
+        
+        try await runSnapshotTest(view: TabBarClearMRoundedSecondaryCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarHasLabelSolidLDividerDefaultCounter() async throws {
+        let appearance = TabBarHasLabelClear.l.divider.default.appearance
+        
+        try await runSnapshotTest(view: TabBarHasLabelSolidLDividerDefaultCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarSolidLDividerSecondarytCounter() async throws {
+        let appearance = TabBar.l.divider.secondary.appearance
+        
+        try await runSnapshotTest(view: TabBarSolidLDividerSecondarytCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarClearLDividerAccenttCounter() async throws {
+        let appearance = TabBarClear.l.divider.accent.appearance
+        
+        try await runSnapshotTest(view: TabBarClearLDividerAccenttCounter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarClearLDividerRoundedDefault() async throws {
+        let appearance = TabBarClear.l.divider.rounded.default.appearance
+        
+        try await runSnapshotTest(view: TabBarClearLDividerRoundedDefault(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarClearLDividerRoundedSecondary() async throws {
+        let appearance = TabBarClear.l.divider.rounded.secondary.appearance
+        
+        try await runSnapshotTest(view: TabBarClearLDividerRoundedSecondary(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarSolidLDividerRoundedAccent() async throws {
+        let appearance = TabBar.l.divider.rounded.accent.appearance
+        
+        try await runSnapshotTest(view: TabBarSolidLDividerRoundedAccent(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarSolidLShadowRoundedAccent() async throws {
+        let appearance = TabBar.l.shadow.rounded.accent.appearance
+        
+        try await runSnapshotTest(view: TabBarSolidLShadowRoundedAccent(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarSolidLShadowRoundedSecondary() async throws {
+        let appearance = TabBar.l.shadow.rounded.secondary.appearance
+        
+        try await runSnapshotTest(view: TabBarSolidLShadowRoundedSecondary(appearance: appearance))
+    }
+    
+    @MainActor
+    func testTabBarSolidLShadowRoundedDefault() async throws {
+        let appearance = TabBar.l.shadow.rounded.default.appearance
+        
+        try await runSnapshotTest(view: TabBarSolidLShadowRoundedDefault(appearance: appearance))
+    }
+}
---
+++ SDDSDemoApp/SDDSDemoAppTests/TabBarTestCase/TabBarTestCases.swift
@@ -0,0 +1,602 @@
+//
+//  TabBarTestCases.swift
+//  SDDSDemoApp
+//
+//  Created by Булинов Ангрик Александрович on 01.09.2025.
+//
+
+import SDDSComponents
+import UIKit
+import SwiftUI
+import SDDSIcons
+
+/**
+ PLASMA-T2182
+ */
+struct TabBarClearLDefaultExtraTypeNone: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2183
+ */
+struct TabBarHasLabelClearMSecondaryCounter: View {
+    var appearance: TabBarAppearance
+    @State var selecetedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selecetedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2184
+ */
+struct TabBarHasLabelSolidLAccentIndicator: View {
+    var appearance: TabBarAppearance
+    @State var selecetedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(indicatorForTest)
+                    )
+                },
+                selectedIndex: $selecetedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2185
+ */
+struct TabBarIslandClearLShadowDefault: View {
+    var appearance: TabBarIslandAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBarIsland(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+
+/**
+ PLASMA-T2186
+ */
+struct TabBarIslandHasLabelClearLShadowSecondaryCounter: View {
+    var appearance: TabBarIslandAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3).ignoresSafeArea()
+            SDDSTabBarIsland(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2187
+ */
+struct TabBarHasLabelSolidLShadowAccentCounter: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2188
+ */
+struct TabBarIslandSolidLDefaultCounter: View {
+    var appearance: TabBarIslandAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        SDDSTabBarIsland(
+            items: (0..<6).map { index in
+                TabBarItemData(
+                    content: AnyView(iconForTest),
+                    selectedContent: AnyView(selectedIcon),
+                    text: "Label",
+                    appearance: appearance.tabBarItemAppearance,
+                    extra: AnyView(counterForTest)
+                )
+            },
+            selectedIndex: $selectedIndex,
+            appearance: appearance
+        )
+    }
+}
+
+/**
+ PLASMA-T2189
+ */
+struct TabBarSolidLRoundedDefaultIndicator: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(indicatorForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2190
+ */
+struct TabBarClearMRoundedSecondaryCounter: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2191
+ */
+struct TabBarHasLabelClearLRoundedAccentCounter: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2192
+ */
+struct TabBarHasLabelSolidLDividerDefaultCounter: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2193
+ */
+struct TabBarSolidLDividerSecondarytCounter: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2194
+ */
+struct TabBarClearLDividerAccenttCounter: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: AnyView(counterForTest)
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2195
+ */
+struct TabBarClearLDividerRoundedDefault: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2196
+ */
+struct TabBarClearLDividerRoundedSecondary: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2197
+ */
+struct TabBarSolidLDividerRoundedAccent: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 1
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2198
+ */
+struct TabBarSolidLShadowRoundedAccent: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2199
+ */
+struct TabBarSolidLShadowRoundedSecondary: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+/**
+ PLASMA-T2200
+ */
+struct TabBarSolidLShadowRoundedDefault: View {
+    var appearance: TabBarAppearance
+    @State var selectedIndex: Int = 0
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.3)
+                .ignoresSafeArea()
+            SDDSTabBar(
+                items: (0..<5).map { index in
+                    TabBarItemData(
+                        content: AnyView(iconForTest),
+                        selectedContent: AnyView(selectedIcon),
+                        text: "Label",
+                        appearance: appearance.tabBarItemAppearance,
+                        extra: nil
+                    )
+                },
+                selectedIndex: $selectedIndex,
+                appearance: appearance
+            )
+        }
+        .padding([.top, .bottom], 12)
+        .frame(height: 150)
+    }
+}
+
+@ViewBuilder
+private var iconForTest: some View {
+    Asset.starOutline36.image
+        .resizable()
+        .renderingMode(.template)
+}
+
+@ViewBuilder
+private var selectedIcon: some View {
+    Asset.starFill36.image
+        .resizable()
+        .renderingMode(.template)
+}
+
+private var counterForTest: SDDSCounter {
+    .init(
+        text: "12",
+        isAnimating: false,
+        isHighlighted: false,
+        isHovered: false,
+        isSelected: false
+    )
+}
+
+private var indicatorForTest: SDDSIndicator {
+    .init()
+}
