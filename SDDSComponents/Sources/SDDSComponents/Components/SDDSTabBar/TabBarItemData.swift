a9a2a1de528c3d4b9faff9f17e15ef0167363cc3
Vladimir Kaltyrin <vkasci@gmail.com>
2025-09-08 12:46:52 +0300

    feat: added contentWidth for tabbaritem

--- SDDSComponents/Sources/SDDSComponents/Components/SDDSTabBar/TabBarItemData.swift
+++ SDDSComponents/Sources/SDDSComponents/Components/SDDSTabBar/TabBarItemData.swift
@@ -6,6 +6,8 @@ public struct TabBarItemData: Identifiable {
     public let content: AnyView?
     public let selectedContent: AnyView?
     public let text: String
+    public let contentWidth: CGFloat?
+    public let allowSelection: Bool
     public let appearance: TabBarItemAppearance?
     public let extra: AnyView?
     public let onTap: (() -> Void)?
@@ -14,6 +16,8 @@ public struct TabBarItemData: Identifiable {
         content: AnyView?,
         selectedContent: AnyView? = nil,
         text: String = "",
+        contentWidth: CGFloat? = nil,
+        allowSelection: Bool = true,
         appearance: TabBarItemAppearance? = nil,
         extra: AnyView? = nil,
         onTap: (() -> Void)? = nil
@@ -21,6 +25,8 @@ public struct TabBarItemData: Identifiable {
         self.content = content
         self.selectedContent = selectedContent
         self.text = text
+        self.contentWidth = contentWidth
+        self.allowSelection = allowSelection
         self.appearance = appearance
         self.extra = extra
         self.onTap = onTap
