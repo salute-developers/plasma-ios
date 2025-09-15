03da3a801b79ffd444fb52acc55a7921bbc9737e
angirb <bulinoff@gmail.com>
2025-09-15 10:24:09 +0300

    test(sdds-acore/testing): Added codeinput snapshot tests

---
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/SDDSServSnapshotTests/CodeInputSnapshotTest.swift
@@ -0,0 +1,72 @@
+//
+//  CodeInputSnapshotTest.swift
+//  SDDSDemoApp
+//
+//  Created by Булинов Ангрик Александрович on 11.09.2025.
+//
+
+@testable import SDSnapshots
+@testable import SDDSDemoApp
+@testable import SDDSComponents
+@testable import SDDSIcons
+import XCTest
+import SDDSServTheme
+import UIKit
+import SwiftUI
+
+final class CodeInputSnapshotTest: XCTestCase {
+    
+    @MainActor
+    func testCodeInputLCodeLengthFourCaptionLeft() async throws {
+        let appearance = CodeInput.l.appearance
+        try await runSnapshotTest(view: CodeInputLCodeLengthFourCaptionLeft(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputMCodeLengthFiveCaptionCenter() async throws {
+        let appearance = CodeInput.m.appearance
+        try await runSnapshotTest(view: CodeInputMCodeLengthFiveCaptionCenter(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputSCodeLengthSix() async throws {
+        let appearance = CodeInput.s.appearance
+        try await runSnapshotTest(view: CodeInputSCodeLengthSix(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputSCodeLengthSixNoCaption() async throws {
+        let appearance = CodeInput.s.appearance
+        try await runSnapshotTest(view: CodeInputSCodeLengthSixNoCaption(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputMCodeLengthSixHidden() async throws {
+        let appearance = CodeInput.m.appearance
+        try await runSnapshotTest(view: CodeInputMCodeLengthSixHidden(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputLInputLetterError() async throws {
+        let appearance = CodeInput.l.appearance
+        try await runSnapshotTest(view: CodeInputLInputLetterError(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputLInputWrongCode() async throws {
+        let appearance = CodeInput.l.appearance
+        try await runSnapshotTest(view: CodeInputLInputWrongCode(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputCorrectCode() async throws {
+        let appearance = CodeInput.l.appearance
+        try await runSnapshotTest(view: CodeInputCorrectCode(appearance: appearance))
+    }
+    
+    @MainActor
+    func testCodeInputCaptionLongText() async throws {
+        let appearance = CodeInput.l.appearance
+        try await runSnapshotTest(view: CodeInputCaptionLongText(appearance: appearance))
+    }
+}
