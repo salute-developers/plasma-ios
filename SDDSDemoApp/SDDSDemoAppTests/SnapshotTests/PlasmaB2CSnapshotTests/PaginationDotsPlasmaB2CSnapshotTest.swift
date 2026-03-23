4ab01bcb2e1fbf81a559e6948742477155e0fe3f
angirb <bulinoff@gmail.com>
2026-03-23 16:23:19 +0300

    test(sdds-core/testing):Paginationdots snapshot tests

--- SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/PlasmaB2CSnapshotTests/PaginationDotsPlasmaB2CSnapshotTest.swift
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/PlasmaB2CSnapshotTests/PaginationDotsPlasmaB2CSnapshotTest.swift
@@ -60,4 +60,69 @@ final class PaginationDotsPlasmaB2CSnapshotTest: PlasmaB2CThemeTestCase {
             )
         )
     }
+    
+    @MainActor
+    func testPaginationDotsVerticalMActiveTypeLineItemTenVisibleSevenPlasmaB2C() async throws {
+        let appearance = PaginationDotsVertical.m.activeTypeLine.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 5,
+                totalCount: 10,
+                visibleCount: 7
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalSItemThreeVisibleThreePlasmaB2C() async throws {
+        let appearance = PaginationDotsHorizontal.s.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 0,
+                totalCount: 3,
+                visibleCount: 3
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalSActiveTypeLinePlasmaB2C() async throws {
+        let appearance = PaginationDotsHorizontal.s.activeTypeLine.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 0,
+                totalCount: 7,
+                visibleCount: 5
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalMItemOneVisibleOnePlasmaB2C() async throws {
+        let appearance = PaginationDotsHorizontal.m.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 0,
+                totalCount: 1,
+                visibleCount: 1
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalMSelectedFivePlasmaB2C() async throws {
+        let appearance = PaginationDotsHorizontal.m.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 5,
+                totalCount: 20,
+                visibleCount: 9
+            )
+        )
+    }
 }
