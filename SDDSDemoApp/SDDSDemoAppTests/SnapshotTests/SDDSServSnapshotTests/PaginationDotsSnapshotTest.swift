7c157797ead352ad54637eef5272a132e502ae80
angirb <bulinoff@gmail.com>
2026-03-23 16:23:19 +0300

    test(sdds-core/testing):Paginationdots snapshot tests

--- SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/SDDSServSnapshotTests/PaginationDotsSnapshotTest.swift
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/SDDSServSnapshotTests/PaginationDotsSnapshotTest.swift
@@ -60,4 +60,70 @@ final class PaginationDotsSnapshotTest: SDDSServThemeTestCase {
             )
         )
     }
+    
+    @MainActor
+    func testPaginationDotsVerticalMActiveTypeLineItemTenVisibleSeven() async throws {
+        let appearance = PaginationDotsVertical.m.activeTypeLine.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 5,
+                totalCount: 10,
+                visibleCount: 7
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalSItemThreeVisibleThree() async throws {
+        let appearance = PaginationDotsHorizontal.s.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 0,
+                totalCount: 3,
+                visibleCount: 3
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalSActiveTypeLine() async throws {
+        let appearance = PaginationDotsHorizontal.s.activeTypeLine.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 0,
+                totalCount: 7,
+                visibleCount: 5
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalMItemOneVisibleOne() async throws {
+        let appearance = PaginationDotsHorizontal.m.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 0,
+                totalCount: 1,
+                visibleCount: 1
+            )
+        )
+    }
+    
+    @MainActor
+    func testPaginationDotsHorizontalMSelectedFive() async throws {
+        let appearance = PaginationDotsHorizontal.m.appearance
+        try await runSnapshotTest(
+            view: PaginationDotsSnapshotCase(
+                appearance: appearance,
+                selectedIndex: 5,
+                totalCount: 20,
+                visibleCount: 9
+            )
+        )
+    }
+    
 }
