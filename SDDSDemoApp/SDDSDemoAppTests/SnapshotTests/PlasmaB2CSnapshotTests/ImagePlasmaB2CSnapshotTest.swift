c9f488514fb136c999fe379274acff60239a4872
angirb <bulinoff@gmail.com>
2026-04-20 14:29:50 +0300

    test(sdds-acore/testing): Image snapshot tests

--- SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/PlasmaB2CSnapshotTests/ImagePlasmaB2CSnapshotTest.swift
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/PlasmaB2CSnapshotTests/ImagePlasmaB2CSnapshotTest.swift
@@ -1,50 +1,59 @@
+//
+//  ImagePlasmaB2CSnapshotTest.swift
+//  SDDSDemoApp
+//
+//  Created by Булинов Ангрик Александрович on 20.04.2026.
+//
+
 @testable import SDSnapshots
 @testable import SDDSDemoApp
 @testable import SDDSComponents
-import SDDSServTheme
-import SwiftUI
+@testable import SDDSIcons
 import XCTest
+import PlasmaB2CTheme
+import UIKit
+import SwiftUI
 
-final class ImageSnapshotTest: SDDSServThemeTestCase {
-
+final class ImagePlasmaB2CSnapshotTest: PlasmaB2CThemeTestCase {
+    
     @MainActor
-    func testImageRatio16x9() async throws {
+    func testImageRatio16x9PlasmaB2C() async throws {
         let appearance = Image.ratio_16_9.appearance
         try await runSnapshotTest(view: ImageRatio16x9Photo(appearance: appearance))
     }
 
     @MainActor
-    func testImageRatio1x1() async throws {
+    func testImageRatio1x1PlasmaB2C() async throws {
         let appearance = Image.ratio_1_1.appearance
         try await runSnapshotTest(view: ImageRatio1x1Photo(appearance: appearance))
     }
 
     @MainActor
-    func testImageRatio1x2() async throws {
+    func testImageRatio1x2PlasmaB2C() async throws {
         let appearance = Image.ratio_1_2.appearance
         try await runSnapshotTest(view: ImageRatio1x2Photo(appearance: appearance))
     }
     
     @MainActor
-    func testImageRatio3x4() async throws {
+    func testImageRatio3x4PlasmaB2C() async throws {
         let appearance = Image.ratio_3_4.appearance
         try await runSnapshotTest(view: ImageRatio3x4Photo(appearance: appearance))
     }
     
     @MainActor
-    func testImageRatio4x3() async throws {
+    func testImageRatio4x3PlasmaB2C() async throws {
         let appearance = Image.ratio_4_3.appearance
         try await runSnapshotTest(view: ImageRatio4x3Photo(appearance: appearance))
     }
     
     @MainActor
-    func testImageRatio9x16() async throws {
+    func testImageRatio9x16PlasmaB2C() async throws {
         let appearance = Image.ratio_9_16.appearance
         try await runSnapshotTest(view: ImageRatio9x16Photo(appearance: appearance))
     }
     
     @MainActor
-    func testImageRatio2x1() async throws {
+    func testImageRatio2x1PlasmaB2C() async throws {
         let appearance = Image.ratio_2_1.appearance
         try await runSnapshotTest(view: ImageRatio2x1Photo(appearance: appearance))
     }
