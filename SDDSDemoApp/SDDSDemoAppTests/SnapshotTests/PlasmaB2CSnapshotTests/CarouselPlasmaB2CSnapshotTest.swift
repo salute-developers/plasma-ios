31e8243b125e5ead6b22226b5a71e5c56d483ad5
angirb <bulinoff@gmail.com>
2026-04-28 16:23:43 +0300

    test(sdds): Added carousel snapshots

--- SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/PlasmaB2CSnapshotTests/CarouselPlasmaB2CSnapshotTest.swift
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/PlasmaB2CSnapshotTests/CarouselPlasmaB2CSnapshotTest.swift
@@ -8,13 +8,14 @@ import UIKit
 
 @MainActor
 final class CarouselPlasmaB2CSnapshotTest: PlasmaB2CThemeTestCase {
+    
     func testCarouselButtonsPlacementInnerPlasmaB2C() async throws {
         let appearance = Carousel.buttonsPlacementInner.appearance
         try await runSnapshotTest(
             view: CarouselSnapshotCase(
                 appearance: appearance,
                 selection: 1,
-                pageCount: 6
+                pageCount: 7
             )
         )
     }
@@ -37,6 +38,66 @@ final class CarouselPlasmaB2CSnapshotTest: PlasmaB2CThemeTestCase {
                 appearance: appearance,
                 selection: 0,
                 pageCount: 4
+            )
+        )
+    }
+    
+    func testCarouselStartButtonsPlacementInnerPlasmaB2C() async throws {
+        let appearance = Carousel.buttonsPlacementInner.appearance
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                pageAlignment: .start,
+            )
+        )
+    }
+        
+    func testCarouselCenterButtonsPlacementOuterPlasmaB2C() async throws {
+        var appearance = Carousel.buttonsPlacementOuter.appearance
+        appearance.size = DefaultCarouselSize(
+            gap: 0,
+            indicatorPadding: appearance.size.indicatorPadding,
+            nextButtonPadding: appearance.size.nextButtonPadding,
+            prevButtonPadding: appearance.size.prevButtonPadding,
+            buttonsPlacement: appearance.size.buttonsPlacement
+        )
+        
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                hasIndicator: false,
+                pageAlignment: .center,
+            )
+        )
+    }
+    
+    func testCarouselEndButtonPlacementInnerPlasmaB2C() async throws {
+        let appearance = Carousel.buttonsPlacementInner.appearance
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                indicatorVisibleCount: 10,
+                pageAlignment: .end,
+            )
+        )
+    }
+    
+    func testCarouselCenterButtonsPlacementInnerPlasmaB2C() async throws {
+        let appearance = Carousel.buttonsPlacementInner.appearance
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                hasIndicator: false,
+                hasControls: false,
+                pageAlignment: .center,
             )
         )
     }
--- SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/SDDSServSnapshotTests/CarouselSnapshotTest.swift
+++ SDDSDemoApp/SDDSDemoAppTests/SnapshotTests/SDDSServSnapshotTests/CarouselSnapshotTest.swift
@@ -8,6 +8,7 @@ import UIKit
 
 @MainActor
 final class CarouselSnapshotTest: SDDSServThemeTestCase {
+    
     func testCarouselButtonsPlacementInner() async throws {
         let appearance = Carousel.buttonsPlacementInner.appearance
         try await runSnapshotTest(
@@ -37,6 +38,66 @@ final class CarouselSnapshotTest: SDDSServThemeTestCase {
                 appearance: appearance,
                 selection: 0,
                 pageCount: 4
+            )
+        )
+    }
+    
+    func testCarouselStartButtonsPlacementInner() async throws {
+        let appearance = Carousel.buttonsPlacementInner.appearance
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                pageAlignment: .start,
+            )
+        )
+    }
+        
+    func testCarouselCenterButtonsPlacementOuter() async throws {
+        var appearance = Carousel.buttonsPlacementOuter.appearance
+        appearance.size = DefaultCarouselSize(
+            gap: 0,
+            indicatorPadding: appearance.size.indicatorPadding,
+            nextButtonPadding: appearance.size.nextButtonPadding,
+            prevButtonPadding: appearance.size.prevButtonPadding,
+            buttonsPlacement: appearance.size.buttonsPlacement
+        )
+        
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                hasIndicator: false,
+                pageAlignment: .center,
+            )
+        )
+    }
+    
+    func testCarouselEndButtonPlacementInner() async throws {
+        let appearance = Carousel.buttonsPlacementInner.appearance
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                indicatorVisibleCount: 10,
+                pageAlignment: .end,
+            )
+        )
+    }
+    
+    func testCarouselCenterButtonsPlacementInner() async throws {
+        let appearance = Carousel.buttonsPlacementInner.appearance
+        try await runSnapshotTest(
+            view: CarouselSnapshotCase(
+                appearance: appearance,
+                selection: 1,
+                pageCount: 10,
+                hasIndicator: false,
+                hasControls: false,
+                pageAlignment: .center,
             )
         )
     }
